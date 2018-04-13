25d9e64d93ede8effe5f9293c72c522adc07ffa4
spencer_dowie <spencer.dowie@uoit.net>
2018-04-13 14:45:24 -0400

    [+]Final Base

---
+++ FinalExam/src/main/java/Main.java
@@ -0,0 +1,17 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application
+{
+
+    public void start(Stage stage)
+    {
+        Stage primaryStage = stage;
+        primaryStage.setTitle("FinalExam");
+    }
+
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+}
